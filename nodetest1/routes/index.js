ead34b0a0261d8f3d5cffc6ed813d0ac72687627
Maricel Mendiola <icelmendiola22@yahoo.com>
2019-08-23 18:51:19 -0700

    Finished adding the input boxes in the AB 109 Client Worksheet.

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -34,30 +34,68 @@ router.post('/adduser', function(req, res) {
     var db = req.db;
 
     // Get our form values. These rely on the "name" attributes
-    var clientFirstName = req.body.firstName;
-    var clientMiddleName = req.body.middleName;
-    var clientLastName = req.body.lastName;
+    var date = req.body.date;
+    var checkbox = req.body.checkbox;
+    var cdcNumber = req.body.cdcNumber;
+    var bookingNumber = req.body.bookingNumber;
+    var name = req.body.name;
     var phoneNumber = req.body.phoneNumber;
     var phoneNumberType = req.body.phoneNumberType;
     var altContactNumber = req.body.altContactNumber;
     var altContactName = req.body.altContactName;
     var altContactRelationship = req.body.altContactRelationship;
     var email = req.body.email;
+    var addressStreet = req.body.addressStreet;
+    var addressCity = req.body.addressCity;
+    var addressState = req.body.addressState;
+    var addressZipcode = req.body.addressZipcode;
+    var birthday = req.body.birthday;
+    var birthCity = req.body.birthCity;
+    var birthState = req.body.birthState;
+    var addressCounty = req.body.addressCounty;
+    var hospitalName = req.body.hospitalName;
+    var birthCountry = req.body.birthCountry;
+    var id = req.body.id;
+    var socialSecurity = req.body.socialSecurity;
+    var program = req.body.program;
+    var timeLeft = req.body.timeLeft;
+    var motherName = req.body.motherName;
+    var fatherName = req.body.fatherName;
+    var notes = req.body.notes;
 
     // Set our collection
     var collection = db.get('usercollection');
 
     // Submit to the DB
     collection.insert({
-        "clientFirstName" : clientFirstName,
-        "clientMiddleName" : clientMiddleName,
-        "clientLastName" : clientLastName,
+        "date" : date,
+        "checkbox" : checkbox,
+        "cdcNumber" : cdcNumber,
+        "bookingNumber" : bookingNumber,
+        "name" : name,
         "phoneNumber" : phoneNumber,
         "phoneNumberType" : phoneNumberType,
         "altContactNumber" : altContactNumber,
         "altContactName" : altContactName,
         "altContactRelationship" : altContactRelationship,
-        "email" : email
+        "email" : email,
+        "addressStreet" : addressStreet,
+        "addressCity" : addressCity,
+        "addressState" : addressState,
+        "addressZipcode" : addressZipcode,
+        "birthday" : birthday,
+        "birthCity" : birthCity,
+        "birthState" : birthState,
+        "addressCounty" : addressCounty,
+        "hospitalName" : hospitalName,
+        "birthCountry" : birthCountry,
+        "id" : id,
+        "socialSecurity" : socialSecurity,
+        "program" : program,
+        "timeLeft" : timeLeft,
+        "motherName" : motherName,
+        "fatherName" : fatherName,
+        "notes" : notes
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
